e2864a75370352550a71933691190cf7714de646
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-12 15:49:16 +0000

    Tue, Sep 12, 2023, 3:49 PM +00:00

--- population/population.c
+++ population/population.c
@@ -4,27 +4,27 @@
 int main(void)
 {
     //Start pop failsafe
-    int start_pop
+    int start_pop;
     do
     {
-        int start_pop = get_int("Start Population: ")
+        int start_pop = get_int("Start Population: ");
     }
     while (start_pop =< 9)
 
     //End pop failsafe
-    int end_pop
+    int end_pop;
     do
     {
-        int end_pop = get_int("Start Population: ")
+        int end_pop = get_int("Start Population: ");
     }
     while (end_pop < start_pop)
 
     //Pop Calculation
-    int i = 0
+    int i = 0;
     while (n < m)
     {
-        n = n + (n/3) + (n/4)
-        i += 1
+        n = n + (n/3) + (n/4)l;
+        i += 1;
     }
-    printf("It took %i years to reach the designated population count.\n", i)
+    printf("It took %i years to reach the designated population count.\n", i);
 }
