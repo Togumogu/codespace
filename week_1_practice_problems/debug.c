2942d751660fbc69e1130448e9194324ac5b27e5
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-13 14:09:35 +0000

    Wed, Sep 13, 2023, 2:09 PM +00:00

--- week_1_practice_problems/debug.c
+++ week_1_practice_problems/debug.c
@@ -1,4 +1,4 @@
-make // Become familiar wih C syntax
+// Become familiar wih C syntax
 // Learn to debug buggy code
 
 #include <cs50.h>
@@ -7,9 +7,9 @@ make // Become familiar wih C syntax
 int main(void)
 {
     // Ask for your name and where live
-    string name = get_string("What is your name? ")
-    string location = get_string("Where do you live? ")
+    string name = get_string("What is your name? ");
+    string location = get_string("Where do you live? ");
 
     // Say hello
-    printf("Hello, %s, from %s!\n", name, location)
+    printf("Hello, %s, from %s!\n", name, location);
 }
