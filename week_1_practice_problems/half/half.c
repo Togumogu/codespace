5d98ab05a0a7b9cdc9a78c2dc6984e7c571c9b11
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-15 13:09:50 +0000

    Fri, Sep 15, 2023, 1:09 PM +00:00

--- week_1_practice_problems/half/half.c
+++ week_1_practice_problems/half/half.c
@@ -12,6 +12,7 @@ int main(void)
     float tax_percent = get_float("Sale Tax Percent: ");
     int tip_percent = get_int("Tip percent: ");
 
+
     printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
 }
 
@@ -20,5 +21,6 @@ float half(float bill, float tax, int tip)
 {
     float bill_tax = bill + (bill * (tax / 100));
     float fin_amount = bill_tax + (bill_tax * (tip / 100));
+    printf("%f\n", fin_amount);
     return fin_amount;
 }
