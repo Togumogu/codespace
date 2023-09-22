6dffc0d7a27809cd4e99f672250c92e961cfeb55
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-22 15:17:46 +0000

    Fri, Sep 22, 2023, 3:17 PM +00:00

--- week_1_pset_1/mario-more/mario.c
+++ week_1_pset_1/mario-more/mario.c
@@ -17,10 +17,6 @@ int main(void)
         {
             printf(".");
         }
-        for (int j = 0; j < n; i++)
-        {
-            printf("#");
-        }
         printf("\n");
     }
     //Space
