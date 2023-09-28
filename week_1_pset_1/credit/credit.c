8d26e703e75ecc31d4194fca89aa7106fa983883
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-28 11:36:09 +0000

    Thu, Sep 28, 2023, 11:36 AM +00:00

--- week_1_pset_1/credit/credit.c
+++ week_1_pset_1/credit/credit.c
@@ -8,15 +8,11 @@ int main(void)
     {
         credit = get_long("Credit Card Number: ");
     }
-    while (credit < 0)
+    while (credit < 0);
 
-    long result10 = credit % 10
-    long result100 = credit % 100
+    long result10 = credit % 10;
+    long result100 = credit % 100;
 
-    printf("%l\n", result10)
-     printf("%l\n", result100)
-
-    //get the second to last digit
-    ((n % 100) - (n % 10)) / 100
-    //sodaki sifiri atmanin yolunu bul
+    printf("%l\n", result10);
+    printf("%l\n", result100);
 }
