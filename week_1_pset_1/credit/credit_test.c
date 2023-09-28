c5eef2d60358d99c1067872179612f383cc12cc0
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-28 14:52:41 +0000

    Thu, Sep 28, 2023, 2:52 PM +00:00

--- week_1_pset_1/credit/credit_test.c
+++ week_1_pset_1/credit/credit_test.c
@@ -14,31 +14,9 @@ int main(void)
     do
     {
         digit1 = (tmp1 % 10) / 10;
-        printf("%i\n", digit2);
-        set2 += digit1
+        set1 += digit1;
 
     }
-    while (tmp2 > 0)
-
-
-    //Second Digits
-    int set2 = 0;
-    long tmp2 = test;
-    int digit2 = 0;
-
-    do
-    {
-        digit2 = (tmp2 % 100) / 10;
-        printf("%i\n", digit2);
-        set2
-
-    }
-    while (tmp2 > 0)
-
-
-
-
-    printf("%lu\n", calc1);
-    printf("%lu\n", calc2);
-    printf("%lu\n", calc3);
+    while (tmp1 > 0);
+    printf("%i\n", set1);
 }
