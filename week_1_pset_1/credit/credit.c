35fbb7d7d69930e618f261ebd3367ea06491b0eb
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-28 11:34:15 +0000

    Thu, Sep 28, 2023, 11:34 AM +00:00

--- week_1_pset_1/credit/credit.c
+++ week_1_pset_1/credit/credit.c
@@ -8,7 +8,13 @@ int main(void)
     {
         credit = get_long("Credit Card Number: ");
     }
-    while ()
+    while (credit < 0)
+
+    long result10 = credit % 10
+    long result100 = credit % 100
+
+    printf("%l\n", result10)
+     printf("%l\n", result100)
 
     //get the second to last digit
     ((n % 100) - (n % 10)) / 100
