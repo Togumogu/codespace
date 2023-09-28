7064a770d46095f78e87bd687bfd52d2a6e41ec2
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-28 15:29:52 +0000

    Thu, Sep 28, 2023, 3:29 PM +00:00

--- week_1_pset_1/credit/credit.c
+++ week_1_pset_1/credit/credit.c
@@ -43,10 +43,19 @@ void checkSum(long credit_num)
     {
         digit1 = num1 % 10;
         num1 = num1 / 100;
-        sum1 += digit1
+        sum1 += digit1;
     }
+    while (num1 > 0);
+
+    //Part 1 - the complex part
+    long num2 = credit_num
+    int set2 = 0;
+    int digit2 = 0;
 
     //Add part 1 & part 2
     int total;
     total = sum1 + sum2;
+
+    //Check if legit
+    if (total % 10 == )
 }
