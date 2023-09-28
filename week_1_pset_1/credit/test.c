f548730a295357c80b2c45adedafd20523ee484f
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-28 15:13:28 +0000

    Thu, Sep 28, 2023, 3:13 PM +00:00

--- week_1_pset_1/credit/test.c
+++ week_1_pset_1/credit/test.c
@@ -3,15 +3,15 @@
 
 int main(void)
 {
-    long func_num = get_long("long?");
+    long func_num = get_long("long?  ");
     int set1 = 0;
     int digit1 = 0;
 
-    printf("funcnum first %lu\n", func_num);
     digit1 = func_num % 10;
     printf("digit1 %i\n", digit1);
-    printf("funcnum second %lu\n", func_num);
+    func_num = func_num / 100;
+    printf("funcnum after 2 digits: %lu\n", func_num);
     set1 += digit1;
-    printf("%i\n", set1);
+    printf("set1: %i\n", set1);
 
 }
