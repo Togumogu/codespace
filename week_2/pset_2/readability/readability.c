2be3d26383e69ee21ccb6e5e5a4eeedb73a97cda
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-30 22:52:11 +0000

    Sat, Sep 30, 2023, 10:52 PM +00:00

--- week_2/pset_2/readability/readability.c
+++ week_2/pset_2/readability/readability.c
@@ -9,4 +9,6 @@ int main(void)
     //count word w space isspace space + 1
     //count sent w . ! ? ispunct punct + 1 however no , ' : ;
     //print grade x
+        //if grade > 16 output 16+
+        //if grade < 1 outout before grade 1
 }
