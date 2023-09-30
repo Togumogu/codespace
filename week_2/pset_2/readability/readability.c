5975302a30dc73926c1e17f8c1f824b66a10342f
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-09-30 23:59:24 +0000

    Sat, Sep 30, 2023, 11:59 PM +00:00

--- week_2/pset_2/readability/readability.c
+++ week_2/pset_2/readability/readability.c
@@ -11,15 +11,13 @@ int main(void)
     string user_input = get_string("Text: ");
     //printf letters
     printf("%i\n", count_letters(user_input));
-
-    //count letter w islower is upper
     //count word w space isspace space + 1
     //count sent w . ! ? ispunct punct + 1 however no , ' : ;
     //print grade x
         //if grade > 16 output 16+
         //if grade < 1 outout before grade 1
 }
-
+// count letters
 int count_letters(string text)
 {
     int letter_no = 0;
@@ -32,3 +30,5 @@ int count_letters(string text)
     }
     return letter_no;
 }
+// count words
+int count_words(string text)
