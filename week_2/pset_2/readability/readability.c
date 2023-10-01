65f71b185693e897def1085580ca1d071769d7e1
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-01 00:56:44 +0000

    Sun, Oct 1, 2023, 12:56 AM +00:00

--- week_2/pset_2/readability/readability.c
+++ week_2/pset_2/readability/readability.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 int count_letters(string user_text);
 int count_words(string text);
@@ -10,14 +10,14 @@ int count_sentences(string text);
 
 int main(void)
 {
-    //ask for input only letters
+    // ask for input only letters
     string user_input = get_string("Text: ");
-    //calculate grade
+    // calculate grade
     float av_letters = (count_letters(user_input) * 100) / count_words(user_input);
     float av_sentences = (count_sentences(user_input) * 100) / count_words(user_input);
     float score = (0.0588 * av_letters) - (0.296 * av_sentences) - 15.8;
     int grade = round(score);
-    //print grade
+    // print grade
     if (grade >= 16)
     {
         printf("Grade 16+\n");
@@ -57,7 +57,7 @@ int count_words(string text)
     }
     return word_no;
 }
-//count sentences
+// count sentences
 int count_sentences(string text)
 {
     int sentence_no = 0;
