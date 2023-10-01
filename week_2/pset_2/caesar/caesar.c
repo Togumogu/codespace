ef15d60462df2f7674605da1a7afafa1a5706f51
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-01 22:41:22 +0000

    Sun, Oct 1, 2023, 10:41 PM +00:00

--- week_2/pset_2/caesar/caesar.c
+++ week_2/pset_2/caesar/caesar.c
@@ -30,7 +30,7 @@ int main(int argc, string argv[])
     int int_key;
     for (int i = 0; i < strlen(argv[1]); i++)
     {
-        int_key = int_key + (argv[1][i] - 48) * pow(10, i);
+        int_key = int_key + (argv[1][i] - 48) * pow(10, );
     }
     //loop to feed every char to replace function
     for (int i = 0; i < strlen(phrase); i++)
