77a3df80628b85e4b12170f55f1f6769fe153c0c
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-01 21:46:16 +0000

    Sun, Oct 1, 2023, 9:46 PM +00:00

--- week_2/pset_2/caesar/caesar.c
+++ week_2/pset_2/caesar/caesar.c
@@ -5,12 +5,14 @@
 
 int main(int argc, string argv[])
 {
+    //failsafe for no or more than 1 inputs
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    for (int i = 0; i < strlen(argv[1]))
+    //failsafe for non
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
         if (!isdigit(argv[1][i]))
         {
