e83371f3174062d329e3b9b51b19c1ba15468b4a
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-02 22:25:12 +0000

    Mon, Oct 2, 2023, 10:25 PM +00:00

--- intro_week_2/pset_2/wordle/wordle.c
+++ intro_week_2/pset_2/wordle/wordle.c
@@ -97,7 +97,10 @@ int main(int argc, string argv[])
 
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
-
+        for (int i= 0; i < wordsize; i++)
+        {
+            status[wordsize]
+        }
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
 
