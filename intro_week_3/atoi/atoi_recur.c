b01c46c5fc818d550a1fdd4b5c8d0f11d4afc061
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-03 20:27:48 +0000

    Tue, Oct 3, 2023, 8:27 PM +00:00

---
+++ intro_week_3/atoi/atoi_recur.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+    convert(input);
+    // Convert string to int
+    printf("%i\n", convert(input));
+}
+
+int convert(string input)
+{
+    // TODO
+    // with recursion
+    int length = strlen(input) - 1
+    int last_digit = input[length] - 48;
+    input[length] = '\0';
+    last_digit = last_digit * pow(10, length)
+
+}
