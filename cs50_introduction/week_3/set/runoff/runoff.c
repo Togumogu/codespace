315b0d1b2c59b9c917f69ebda955f6a4ab473124
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-04 23:29:23 +0000

    Wed, Oct 4, 2023, 11:29 PM +00:00

--- cs50_introduction/week_3/set/runoff/runoff.c
+++ cs50_introduction/week_3/set/runoff/runoff.c
@@ -154,9 +154,9 @@ void tabulate(void)
         for (int j = 0; j < candidate_count; j++)
         {
             // find smallest rank to find voter preference
-            if (preferences[i][j] == )
+            if (preferences[i][j] < first_rank)
             {
-                first_rank = preferences[i][j];
+                first_rank = j;
             }
         }
     }
