9dbd380c197e70af9f7a7e7e79d8c4f160e1ab7e
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-04 23:43:00 +0000

    Wed, Oct 4, 2023, 11:43 PM +00:00

--- cs50_introduction/week_3/set/runoff/runoff.c
+++ cs50_introduction/week_3/set/runoff/runoff.c
@@ -175,7 +175,12 @@ bool print_winner(void)
     // iterate through candidates
     for (int i = 0; i < candidate_count; i++)
     {
+        // if one of candidate votes bigger than half of the votes
         if (candidates[i].votes > voter_count / 2)
+        {
+            printf("Winner: %s!\n", candidates[i].name);
+            return true;
+        }
     }
     return false;
 }
