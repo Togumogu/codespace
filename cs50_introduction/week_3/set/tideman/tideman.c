21ef6eefc5f1af0faa238e9dd7c803f9e85522cd
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-05 20:53:39 +0000

    Thu, Oct 5, 2023, 8:53 PM +00:00

--- cs50_introduction/week_3/set/tideman/tideman.c
+++ cs50_introduction/week_3/set/tideman/tideman.c
@@ -134,6 +134,16 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (i != j)
+            {
+                
+            }
+        }
+    }
     return;
 }
 
