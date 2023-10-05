a0e36ad91f0f8733968e74b6efe31e74e8d11b07
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-05 21:33:12 +0000

    Thu, Oct 5, 2023, 9:33 PM +00:00

--- cs50_introduction/week_3/set/tideman/tideman.c
+++ cs50_introduction/week_3/set/tideman/tideman.c
@@ -174,15 +174,16 @@ void sort_pairs(void)
     // iterate through pairs
     for (int i = 0; i < pair_count; i++)
     {
-        if ((preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner]) < (preferences[pairs[i + 1].winner][pairs[i + 1].loser] - preferences[pairs[i + 1].loser][pairs[i + 1].winner]))
+        for (int j = i + 1; j < pair_count; j++)
         {
-            pair tmp = pairs[i];
-            pairs[i] = pairs[i + 1];
-            pairs[i + 1] = tmp;
+            if ((preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner]) < (preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].loser][pairs[j].winner]))
+            {
+                pair tmp = pairs[i];
+                pairs[i] = pairs[j];
+                pairs[j] = tmp;
+            }
         }
-        // find the most difference between votes
     }
-
     return;
 }
 
