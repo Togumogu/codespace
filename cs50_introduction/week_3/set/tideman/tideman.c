dbf45d546122cde2b9b76e3792de6daaa570bccf
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-05 21:45:03 +0000

    Thu, Oct 5, 2023, 9:45 PM +00:00

--- cs50_introduction/week_3/set/tideman/tideman.c
+++ cs50_introduction/week_3/set/tideman/tideman.c
@@ -136,28 +136,20 @@ void add_pairs(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j++)
         {
-            if (storage > pair_count - 1)
+            if (preferences[i][j] > preferences[j][i])
             {
-                printf("Error\n");
-                return;
-            }
-            if (i != j)
-            {
-                if (preferences[i][j] > preferences[j][i])
-                {
-                    pairs[pair_count].winner = i;
-                    pairs[pair_count].loser = j;
-                    pair_count++;
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
 
-                }
-                else if (preferences[i][j] < preferences[j][i])
-                {
-                    pairs[pair_count].winner = j;
-                    pairs[pair_count].loser = i;
-                    pair_count++;
-                }
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
             }
         }
     }
