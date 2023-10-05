a8af1cec4fc36e32a458d3bd9d614da9169fd1a8
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-05 23:54:03 +0000

    Thu, Oct 5, 2023, 11:54 PM +00:00

--- cs50_introduction/week_3/set/tideman/tideman.c
+++ cs50_introduction/week_3/set/tideman/tideman.c
@@ -228,6 +228,15 @@ bool is_cycle(int winner, int loser)
 // Print the winner of the election
 void print_winner(void)
 {
+    for (int i = 0; i < candidate_count; i++)
+    {
+        bool is_winner = false;
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if()
+        }
+    }
+    /* THIS WORKS TOO BUT CHECK50 ISN'T GIVING ALL GREEN
     // create array for who has the most locks
     int votes[candidate_count];
     // populate votes[]
@@ -266,4 +275,5 @@ void print_winner(void)
         }
     }
     return;
+    */
 }
