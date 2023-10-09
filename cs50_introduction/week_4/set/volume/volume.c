ae8bdda3fe24a36954db227f89716f871ae82ac0
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-09 22:13:15 +0000

    Mon, Oct 9, 2023, 10:13 PM +00:00

--- cs50_introduction/week_4/set/volume/volume.c
+++ cs50_introduction/week_4/set/volume/volume.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
     }
 
     // TODO: Read samples from input file and write updated data to output file
-
+    for int i = 44; i < 
     // Close files
     fclose(input);
     fclose(output);
