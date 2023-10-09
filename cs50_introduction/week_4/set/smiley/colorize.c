dca1e6b9157cb49196b58d146bdc91053f75890c
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-09 21:41:01 +0000

    Mon, Oct 9, 2023, 9:41 PM +00:00

--- cs50_introduction/week_4/set/smiley/colorize.c
+++ cs50_introduction/week_4/set/smiley/colorize.c
@@ -71,22 +71,7 @@ int main(int argc, char *argv[])
     for (int i = 0; i < height; i++)
     {
         // read row into pixel array
-        fread(image[i], sizeof(RGBTRIPLE), width, inptr);#include "helpers.h"
-
-void colorize(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            // Make black pixels turn red
-            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
-            {
-                image[i][j].rgbtRed = 0xff;
-            }
-        }
-    }
-}
+        fread(image[i], sizeof(RGBTRIPLE), width, inptr);
 
         // skip over padding
         fseek(inptr, padding, SEEK_CUR);
@@ -124,3 +109,24 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
 
     return 0;
 }
+
+/*
+#include "helpers.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Make black pixels turn red
+            if (image[i][j].rgbtBlue == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtRed == 0x00)
+            {
+                image[i][j].rgbtBlue = 0xAA;
+                image[i][j].rgbtGreen = 0xFF;
+                image[i][j].rgbtRed = 0x00;
+            }
+        }
+    }
+}
+*/
