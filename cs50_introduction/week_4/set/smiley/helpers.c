5c83d08851980fe3bc31f0f8ab30bb0910d49bbe
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-09 21:21:50 +0000

    Mon, Oct 9, 2023, 9:21 PM +00:00

--- cs50_introduction/week_4/set/smiley/helpers.c
+++ cs50_introduction/week_4/set/smiley/helpers.c
@@ -7,8 +7,10 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            char *address = malloc(sizeof(image + 1));
-            printf("%s\n", address);
+            if (image[i][j].rgbtBlue == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtRed == 0x00)
+            {
+                
+            }
         }
     }
 }
