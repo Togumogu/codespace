69c47ca172794e2746026f746b0d89cdc00cc94b
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-09 21:34:51 +0000

    Mon, Oct 9, 2023, 9:34 PM +00:00

--- cs50_introduction/week_4/set/smiley/colorize.c
+++ cs50_introduction/week_4/set/smiley/colorize.c
@@ -71,7 +71,22 @@ int main(int argc, char *argv[])
     for (int i = 0; i < height; i++)
     {
         // read row into pixel array
-        fread(image[i], sizeof(RGBTRIPLE), width, inptr);
+        fread(image[i], sizeof(RGBTRIPLE), width, inptr);#include "helpers.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Make black pixels turn red
+            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            {
+                image[i][j].rgbtRed = 0xff;
+            }
+        }
+    }
+}
 
         // skip over padding
         fseek(inptr, padding, SEEK_CUR);
