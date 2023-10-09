112feba61264ce0b2652b7004b50574820b832b7
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-09 23:54:21 +0000

    Mon, Oct 9, 2023, 11:54 PM +00:00

--- cs50_introduction/week_4/set/volume/volume.c
+++ cs50_introduction/week_4/set/volume/volume.c
@@ -41,13 +41,13 @@ int main(int argc, char *argv[])
     // write into output header
     fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
     // TODO: Read samples from input file and write updated data to output file
-    uint16_t sample;
+    int16_t sample;
     // read into input sample
-    while (fread(&sample, sizeof(uint16_t), 1, input))
+    while (fread(&sample, sizeof(int16_t), 1, input))
     {
         // write into output sample
         sample *= factor;
-        fwrite(&sample, sizeof(uint16_t), 1, output);
+        fwrite(&sample, sizeof(int16_t), 1, output);
     }
     // Close files
     fclose(input);
