df3445f66c87d837e3d6489b35c809fd6477cd09
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-09 23:28:01 +0000

    Mon, Oct 9, 2023, 11:28 PM +00:00

--- cs50_introduction/week_4/set/volume/volume.c
+++ cs50_introduction/week_4/set/volume/volume.c
@@ -38,9 +38,9 @@ int main(int argc, char *argv[])
     // create an array of 44 each element able to store 8 bits or 1 byte so 44 bytes in total - uint8_t stores 8 bits
     uint8_t header[HEADER_SIZE];
     // read into input header
-    fread(header, sizeof(uint8_t), 1, input);
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
     // write into output header
-    fwrite(header, sizeof(uint8_t), 1, output);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
     // TODO: Read samples from input file and write updated data to output file
     uint16_t sample[sizeof(input - 44)];
     // read into input header
