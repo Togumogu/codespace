5f72781ad49d1b1d1c06cf41a848469d0ffbe836
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-17 23:35:58 +0000

    Tue, Oct 17, 2023, 11:35 PM +00:00

--- cs50_introduction/week_4/set/filter-less/helpers.c
+++ cs50_introduction/week_4/set/filter-less/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            BYTE grayscaled = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+            BYTE grayscaled = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
             image[i][j].rgbtBlue = grayscaled;
             image[i][j].rgbtGreen = grayscaled;
             image[i][j].rgbtRed = grayscaled;
@@ -28,6 +28,18 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             temp.rgbtBlue = image[i][j].rgbtBlue;
             temp.rgbtGreen = image[i][j].rgbtGreen;
             temp.rgbtRed = image[i][j].rgbtRed;
+            if (image[i][j].rgbtRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+            if (image[i][j].rgbtBlue > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+            if (image[i][j].rgbtGreen > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
             image[i][j].rgbtRed = round(.393 * temp.rgbtRed) + round(.769 * temp.rgbtGreen) + round(.189 * temp.rgbtBlue);
             image[i][j].rgbtGreen = round(.349 * temp.rgbtRed) + round(.686 * temp.rgbtGreen) + round(.168 * temp.rgbtBlue);
             image[i][j].rgbtBlue = round(.272 * temp.rgbtRed) + round(.534 * temp.rgbtGreen) + round(.131 * temp.rgbtBlue);
