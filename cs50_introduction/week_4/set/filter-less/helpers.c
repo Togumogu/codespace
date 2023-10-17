ce79c0226ac1fd8fd2b979825504b0f2ede07f67
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-17 22:48:46 +0000

    Tue, Oct 17, 2023, 10:48 PM +00:00

--- cs50_introduction/week_4/set/filter-less/helpers.c
+++ cs50_introduction/week_4/set/filter-less/helpers.c
@@ -15,10 +15,21 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     }
     return;
 }
-
+/*
+  sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+  sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+  sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+  */
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE
+        }
+    }
     return;
 }
 
