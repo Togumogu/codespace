736d9b623801829340f2d6b26ad7912cd926e4e5
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-10-20 14:40:25 +0000

    Fri, Oct 20, 2023, 2:40 PM +00:00

--- cs50_introduction/week_4/set/filter-less/helpers.c
+++ cs50_introduction/week_4/set/filter-less/helpers.c
@@ -8,6 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            //av of each rgb element
             BYTE grayscaled = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
             image[i][j].rgbtBlue = grayscaled;
             image[i][j].rgbtGreen = grayscaled;
@@ -27,7 +28,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float red = image[i][j].rgbtRed;
             float blue = image[i][j].rgbtBlue;
             float green = image[i][j].rgbtGreen;
-
+            //either 255 or the other value which ever is min
             image[i][j].rgbtRed = fmin(255, round(.393 * red + .769 * green + .189 * blue));
             image[i][j].rgbtGreen = fmin(255, round(.349 * red + .686 * green + .168 * blue));
             image[i][j].rgbtBlue = fmin(255, round(.272 * red + .534 * green + .131 * blue));
@@ -41,10 +42,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             RGBTRIPLE temp[height][width];
+            temp[i][j] = image[i][j];
             image[i][j] = image[i][width - j];
+            image[i][width - j] = temp[i][j];
         }
     }
     return;
@@ -53,5 +56,37 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //populate copy
+            copy[i][j] = image[i][j];
+        }
+    }
+    // count again
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+            float blue = round((copy[i - 1][j - 1].rgbtBlue + copy[i - 1][j].rgbtBlue + copy[i - 1][j + 1].rgbtBlue
+                                + copy[i][j - 1].rgbtBlue + copy[i][j].rgbtBlue + copy[i][j + 1].rgbtBlue
+                                + copy[i + 1][j - 1].rgbtBlue + copy[i + 1][j].rgbtBlue + copy[i + 1][j + 1].rgbtBlue) / 9);
+
+            float red = round((copy[i - 1][j - 1].rgbtRed + copy[i - 1][j].rgbtRed + copy[i - 1][j + 1].rgbtRed
+                                + copy[i][j - 1].rgbtRed + copy[i][j].rgbtRed + copy[i][j + 1].rgbtRed
+                                + copy[i + 1][j - 1].rgbtRed + copy[i + 1][j].rgbtRed + copy[i + 1][j + 1].rgbtRed) / 9);
+
+            float green = round((copy[i - 1][j - 1].rgbtGreen + copy[i - 1][j].rgbtGreen + copy[i - 1][j + 1].rgbtGreen
+                                + copy[i][j - 1].rgbtGreen + copy[i][j].rgbtGreen + copy[i][j + 1].rgbtGreen
+                                + copy[i + 1][j - 1].rgbtGreen + copy[i + 1][j].rgbtGreen + copy[i + 1][j + 1].rgbtGreen) / 9);
+
+            image[i][j].rgbtBlue = blue;
+            image[i][j].rgbtGreen = green;
+            image[i][j].rgbtRed = red;
+        }
+    }
     return;
 }
