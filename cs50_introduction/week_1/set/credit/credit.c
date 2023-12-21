09cfd2f7f4c804512f318aee59e303c53d330fde
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-12-21 23:51:48 +0000

    Thu, Dec 21, 2023, 11:51 PM +00:00

--- cs50_introduction/week_1/set/credit/credit.c
+++ cs50_introduction/week_1/set/credit/credit.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    long credit_num;
+    do
+    {
+        credit_num = get_long("Credit Card Number: ");
+    }
+    while (credit)
 }
