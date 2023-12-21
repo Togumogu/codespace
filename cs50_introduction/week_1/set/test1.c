a32ebf5a22a971de733a082fbb241d8c4c061159
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-12-21 23:32:49 +0000

    Thu, Dec 21, 2023, 11:32 PM +00:00

---
+++ cs50_introduction/week_1/set/test1.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Tower Height: ");
+    }
+    while (1 >= height || height >= 12);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = height; j > 0; j--)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < height; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
