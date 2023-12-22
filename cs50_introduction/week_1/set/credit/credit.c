e4410e7b6e98a5986bd68ab657a85344ac1a33cc
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-12-22 00:16:16 +0000

    Fri, Dec 22, 2023, 12:16 AM +00:00

--- cs50_introduction/week_1/set/credit/credit.c
+++ cs50_introduction/week_1/set/credit/credit.c
@@ -11,13 +11,24 @@ int main(void)
     while (credit_num < 0);
     //calculate length
     int length = 0;
-    for (int i = 0; 0 < credit_num ; i++)
+    for (int i = 0; 0 < credit_num; i++)
     {
         credit_num = credit_num / 10;
         length++;
     }
+    //if card length not 13, 15 or 16 - quit out
     if (length != 15 && length != 16 && length != 13)
     {
         printf("INVALID\n");
+    }
+    else
+    {
+        int first_calc = 0;
+        int second_calc = 0;
+        for (int i = 0; 0 < credit_num; i++)
+        {
+            if (i % 2 = 0)
+        }
+
     }
 }
