e66797f6c0bb16a735da5fb3c9ee27779e1999f0
Togumogu <111085256+Togumogu@users.noreply.github.com>
2023-12-22 00:01:18 +0000

    Fri, Dec 22, 2023, 12:01 AM +00:00

--- cs50_introduction/week_1/set/credit/credit.c
+++ cs50_introduction/week_1/set/credit/credit.c
@@ -3,18 +3,12 @@
 
 int main(void)
 {
-
-    int n = 5467;
-    int y;
-    y = 5467 / 10;
-    printf("Final: %i\n", y);
-    /*
     long credit_num;
     do
     {
         credit_num = get_long("Credit Card Number: ");
     }
     while (credit)
-    */
     //calculate lenght
+    for (int i = 0; i )
 }
